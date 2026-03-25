b2423721c153021d0a9ddbf1ba14c2e5c7a407bb
Rojan Aryal <rojanaryal@Rojans-MacBook-Air.local>
2026-03-25 17:11:38 +0545

    fix:restructure

--- src/reducers/cart/types.ts
+++
@@ -1,9 +0,0 @@
-enum Types {
-  bulkAdd = "BULKADD",
-  addToCart = "ADDTOCART",
-  removeSingleItem = "REMOVESINGLEITEM",
-  removeWholeProduct = "REMOVEWHOLEPRODUCT",
-  removeAllItems = "REMOVEALLITEMS"
-}
-
-export default Types;
